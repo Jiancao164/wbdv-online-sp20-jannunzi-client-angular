1df4bcb933853084a31c8c2d96a975285b790449
abc <abc@example.com>
2020-03-22 09:54:37 -0700

    change server link

--- src/app/services/CourseServiceClient.ts
+++ src/app/services/CourseServiceClient.ts
@@ -3,9 +3,9 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class CourseServiceClient {
   findAllCourses = () =>
-    fetch('http://wbdv-generic-server.herokuapp.com/api/001358744/courses')
+    fetch('http://wbdv-generic-server.herokuapp.com/api/jannunzi/courses')
       .then(response => response.json())
   findCourseById = (courseId) =>
-    fetch(`http://wbdv-generic-server.herokuapp.com/api/001358744/courses/${courseId}`)
+    fetch(`http://wbdv-generic-server.herokuapp.com/api/jannunzi/courses/${courseId}`)
       .then(response => response.json())
 }
--- src/app/services/LessonServiceClient.ts
+++ src/app/services/LessonServiceClient.ts
@@ -3,6 +3,6 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class LessonServiceClient {
   findLessonsForModule = moduleId =>
-    fetch(`https://wbdv-generic-server.herokuapp.com/api/001358744/modules/${moduleId}/lessons`)
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/jannunzi/modules/${moduleId}/lessons`)
       .then(response => response.json())
 }
